5e8972286ac2cbc2618524c90345a49473b92966
lidimi <lidsheq97@gmail.com>
2023-05-22 22:49:51 +0200

    sass stylesheet

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,11 @@
 function Navbar() {
     return (
     <div className="navbar">
-        <h1 className="navbar__title"><span className="title-notes">Notes</span> & Todo List</h1>
+        <h1 className="title"><span className="title__notes">Notes</span> & <span className="title__list">Todo List</span></h1>
+        <div className="navbar__buttons">
         <button className="navbar__button">Add Note</button>
+        <button className="navbar__button">Add TODO</button>
+        </div>
     </div>
     )
 }
