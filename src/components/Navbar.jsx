780f30bd05ccc9ccb2b6cd09f5a0783c44e002a3
lidimi <lidsheq97@gmail.com>
2023-05-22 21:17:35 +0200

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+
+function Navbar() {
+    return (
+    <div className="navbar">
+        <h1 className="navbar__title"><span className="title-notes">Notes</span> & Todo List</h1>
+        <button className="navbar__button">Add Note</button>
+    </div>
+    )
+}
+
+export default Navbar
